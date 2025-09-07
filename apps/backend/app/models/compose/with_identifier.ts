521533f07d185ad195cf406f32e99ac64fbd383d
epenflow <epenflow@gmail.com>
2025-09-07 21:09:39 +0800

    add default name WithTimestamp and WithIdentifier

--- apps/backend/app/models/compose/with_identifier.ts
+++ apps/backend/app/models/compose/with_identifier.ts
@@ -3,7 +3,9 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export type IdentifierContract = string | number
 
-export default function <T extends NormalizeConstructor<typeof BaseModel>>(superclass: T) {
+export default function WithIdentifier<T extends NormalizeConstructor<typeof BaseModel>>(
+  superclass: T
+) {
   class BaseClass extends superclass {
     @column({ isPrimary: true })
     declare id: IdentifierContract
--- apps/backend/app/models/compose/with_timestamp.ts
+++ apps/backend/app/models/compose/with_timestamp.ts
@@ -2,7 +2,9 @@ import { NormalizeConstructor } from '@adonisjs/core/types/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 
-export default function <T extends NormalizeConstructor<typeof BaseModel>>(superclass: T) {
+export default function WithTimestamp<T extends NormalizeConstructor<typeof BaseModel>>(
+  superclass: T
+) {
   class BaseClass extends superclass {
     @column.dateTime({ autoCreate: true })
     declare createdAt: DateTime
